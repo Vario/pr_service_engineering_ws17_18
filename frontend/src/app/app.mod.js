299479724bc853666f239807f36d13d51094e395
Werner Richtsfeld <w.richtsfeld@gmx.com>
2018-01-12 19:58:10 +0100

    + fixed and moved print directive to apieval.directives module

--- frontend/src/app/app.mod.js
+++ frontend/src/app/app.mod.js
@@ -10,10 +10,10 @@
             'apieval.services',
             'apieval.dashboard',
             'apieval.loading',
+            'apieval.directives',
             'apieval.sanitize',
             'ngFileUpload',
             'ngDialog',
-            'ngSanitize',
-            'ngPrint'
+            'ngSanitize'
         ]);
 })(angular);
