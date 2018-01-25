80be6977b69e9a907e02082a16898880e9afc72a
Patrick Buchner <patrick.buchner@criteriamx.com>
2018-01-25 16:27:17 +0100

    Docker Integration

--- backend/spring-server/src/main/java/at/jku/se/pr/rest/qualityapi/mongodb/MongoClientFactory.java
+++ backend/spring-server/src/main/java/at/jku/se/pr/rest/qualityapi/mongodb/MongoClientFactory.java
@@ -9,7 +9,11 @@ public class MongoClientFactory {
 
     public static MongoClient getMongoClient() {
         if(mongoClient == null){
-            mongoClient = new MongoClient( "localhost" , 27017 );
+            System.out.println("NEW MONGOCLIENT INSTANCE");
+            String mongoDbHostname = System.getenv("JKU_REST_QUALITY_API_MONGO_DB_HOSTNAME");
+            if(mongoDbHostname == null)
+                mongoDbHostname = "localhost";
+            mongoClient = new MongoClient( mongoDbHostname , 27017 );
         }
         return mongoClient;
     }
