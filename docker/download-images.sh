ec82e328d8a2b5d19580b89f9c2d2ec4c426c3d4
Patrick Buchner <patrick.buchner@criteriamx.com>
2018-01-26 21:51:31 +0100

    docker

---
+++ docker/download-images.sh
@@ -0,0 +1,6 @@
+set -e
+
+sudo docker pull 689213771070.dkr.ecr.eu-central-1.amazonaws.com/rest-quality-api:rest-quality-api
+sudo docker pull 689213771070.dkr.ecr.eu-central-1.amazonaws.com/rest-quality-api:comparison-api
+sudo docker pull 689213771070.dkr.ecr.eu-central-1.amazonaws.com/rest-quality-api:violation-api
+sudo docker pull 689213771070.dkr.ecr.eu-central-1.amazonaws.com/rest-quality-api:frontend
