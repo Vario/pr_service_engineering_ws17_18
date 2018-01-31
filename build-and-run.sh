b1656ccab80aa0c8483b5786b2d2270eea3092af
Patrick Buchner <patrick.buchner@criteriamx.com>
2018-01-31 11:33:15 +0100

    deployment script

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,8 +2,14 @@ set -e
 
 currdir=$(pwd)
 
+# Building Zally
+cd ../zally/server
+./gradlew clean build
+
+cd $currdir
 cd backend/spring-server
 
+# Building Backend
 mvn package
 sudo docker-compose build
 sudo docker-compose up
